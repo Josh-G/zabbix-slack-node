a60bb0809505da805744c490812070fd6e50447b
Josh Grant <josh@grantj.io>
2016-12-16 21:37:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,88 @@
+var IncomingWebhook = require('@slack/client').IncomingWebhook;
+
+var url = process.env.SLACK_WEBHOOK_URL || '***REMOVED***';
+
+var to = process.argv[2];
+var title = process.argv[3];
+var payload = process.argv[4];
+
+var parser = /^([\d\w ]+?):(.+?)$/gm;
+parser = new RegExp(parser);
+
+var tags = {};
+while ((result = parser.exec(payload)) !== null) {
+    tags[result[1].toLowerCase().trim()] = result[2].trim();
+}
+tags['when'] = new Date(tags['when']);
+
+console.log(tags);
+
+var color = '#59db8f';
+if (tags['status'] == 'PROBLEM') {
+    if (tags['severity'] == 'Warning') {
+        color = '#FFC859';
+    }
+    else if (tags['severity'] == 'Average') {
+        color = '#FFA059';
+    }
+}
+
+var msg_title = 'Unknown Event!';
+var icon = ':grey_question:';
+if (tags['status'] == 'OK') {
+    icon = ':heavy_check_mark:';
+    msg_title = 'Resolved: '+tags['trigger']
+}
+else {
+    if (tags['severity'] == 'Disaster' || tags['Severity'] == 'High' || tags['severity'] == 'Average') {
+        icon = ':bangbang:';
+    }
+    else {
+        icon = ':exclamation:';
+    }
+    msg_title = 'PROBLEM: '+tags['trigger']
+}
+
+var response = {
+        username: 'zabbix',
+        // channel: '#test',
+        channel: to,
+        icon_emoji: icon,
+        attachments: [{
+            title: msg_title,
+            title_link: 'http://zabbix/events.php?filter_set=1&triggerid='+tags['trigger id'],
+            color: color,
+            fields: [
+                {
+                    title: "Trigger",
+                    value: tags['trigger'],
+                    short: true,
+                },
+                {
+                    title: "Severity",
+                    value: tags['severity'],
+                    short: true,
+                },
+                {
+                    title: "Current Value",
+                    value: tags['value'],
+                    short: true,
+                },
+                {
+                    title: "IP Address",
+                    value: tags['ip'],
+                    short: true,
+                },
+                {
+                    title: "Graph Link",
+                    value: '<http://zabbix/history.php?action=showgraph&itemids%5B%5D='+tags['item id']+'|Click Me>',
+                    short: true,
+                },
+            ]
+        }],
+};
+
+var webhook = new IncomingWebhook(url, {iconEmoji: icon});
+webhook.send(response, function() {
+
+});
---
+++ slack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+
+cd "$parent_path"
+
+to=$1
+subject=$2
+body=$3
+
+node ./index.js "#test" "OK: Processor load is too high on Zabbix Local" "OK: Processor load is too high on Zabbix Local,Host: Zabbix Local
+Event: system.cpu.load[percpu,avg1]
+Item ID: 23296
+Event ID: 19659
+Trigger ID: 13497
+Severity: Warning
+Status: OK
+Trigger: Processor load is too high on Zabbix Local
+Ack: No
+Service: Zabbix servers
+Value: 0.17
+Text: OK: Processor load is too high on Zabbix Local
+Tags: {EVENT.TAGS}
+IP: 127.0.0.1
+Template: Template OS Linux: {Zabbix Local:system.cpu.load[percpu,avg1].avg(5m)}>2
+When: 2016.12.16 21:32:16 "
